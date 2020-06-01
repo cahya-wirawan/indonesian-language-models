666c3dab68bd5c2fa704f545569086c04d66146c
Cahya Wirawan <Cahya.Wirawan@gmail.com>
2020-06-01 19:05:28 +0200

    added gpt2

---
+++ Transformers/GPT2/run_lm_train.py
@@ -0,0 +1,58 @@
+from simpletransformers.language_modeling import LanguageModelingModel
+import logging
+
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
+
+train_args = {
+    "reprocess_input_data": False,
+    "overwrite_output_dir": True,
+    "num_train_epochs": 3,
+    "save_eval_checkpoints": True,
+    "save_model_every_epoch": False,
+    "learning_rate": 5e-4,
+    "warmup_steps": 10000,
+    "train_batch_size": 64,
+    "eval_batch_size": 128,
+    "gradient_accumulation_steps": 1,
+    "block_size": 128,
+    "max_seq_length": 128,
+    "dataset_type": "simple",
+    "wandb_project": "GPT2 - Indonesian",
+    "wandb_kwargs": {"name": "GPT2-SMALL"},
+    "logging_steps": 100,
+    "evaluate_during_training": True,
+    "evaluate_during_training_steps": 50000,
+    "evaluate_during_training_verbose": True,
+    "use_cached_eval_features": True,
+    "sliding_window": True,
+    "vocab_size": 52000,
+    "generator_config": {
+        "embedding_size": 128,
+        "hidden_size": 256,
+        "num_hidden_layers": 3,
+    },
+    "discriminator_config": {
+        "embedding_size": 128,
+        "hidden_size": 256,
+    },
+    "fp16": False,
+}
+
+train_file = ["data/train.txt", "data/valid.txt"]
+test_file = "data/test.txt"
+
+model = LanguageModelingModel(
+    "gpt2",
+    None,
+    args=train_args,
+    train_files=train_file,
+)
+
+
+model.train_model(
+    train_file, eval_file=test_file,
+)
+
+model.eval_model(test_file)
